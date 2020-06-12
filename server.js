f13979b1a04cc68be9404148c577e7ad2cf978ef
Yash Velagapudi <yashv6655@gmail.com>
2020-06-11 21:07:34 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// Importing Modules
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+// importing files
+const routes = require("./routes");
+
+// Define Global Variables
+const app = express();
+const log = console.log;
+const PORT = process.env.PORT || 8080; // Step 1
+
+// Step 2
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://mern-tutorial:HfToABP4IBYn8NYS@accounts-yr3cj.azure.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+// Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", routes);
+app.use(cors({ origin: "http://localhost:3000" }));
+
+// Step 3
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
+
+app.listen(PORT, () => {
+  log(`Server is starting at PORT: ${PORT}`);
+});
